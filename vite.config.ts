0cddc17c71422d545a041148bb5c677742df4760
Igor Ieromenko <igorrooney@gmail.com>
2026-07-01 20:59:51 +0100

    Add GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/reusable-front-end-delivery-playbook-demo/',
   plugins: [react(), tailwindcss()],
 })
